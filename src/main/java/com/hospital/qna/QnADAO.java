e8be189b89b1d482934d1f209809440b2c912252
hyonna <yha0118@naver.com>
2019-08-06 09:28:09 +0900

    cc

---
+++ src/main/java/com/hospital/qna/QnADAO.java
@@ -0,0 +1,65 @@
+package com.hospital.qna;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.hospital.board.BoardDAO;
+import com.hospital.board.BoardVO;
+import com.hospital.util.PageMaker;
+
+@Repository
+public class QnADAO implements BoardDAO{
+	
+	@Inject
+	private SqlSession sqlSession;
+	private static final String NAMESPACE="QnaMapper.";
+
+	@Override
+	public int getTotalCount(PageMaker pageMaker) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setWrite(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setUpdate(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDelete(int num) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public BoardVO getSelect(int num) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardVO> getList(PageMaker pageMaker) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addHit() throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
