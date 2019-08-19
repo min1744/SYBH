ef97a6b537e9ecf9569e5705d1ade1846eb90495
hyonna <yha0118@naver.com>
2019-08-19 12:17:05 +0900

    ccc

--- src/main/webapp/resources/js/board/boardSelect.js
+++ src/main/webapp/resources/js/board/boardSelect.js
@@ -1,33 +1,32 @@
-	$(function() {
-		var board = $("#board").val();
-						
+$(function() {
+		
 		$("#delete").click(function() {
 			var result = confirm("삭제하시겠습니까?");
-			var num = $("#num").val();
 			if(result){
-				post({'num':num});
+				
+				location.href="./boardList";
 			}
+						
 		});
 		
-		console.log($('#qnum').val());
 		
-		//qna 부분
+		/////////////////////////////////qna 원본 글 삭제 관련▼
 		$("#q_delete").click(function() {
 			var result = confirm("삭제하시겠습니까?");
-			var num = $('#qnum').val();
+			var ref = $('#qref').val();
 			var menu = $('#qmenu').val();
 			if(result){
 				
-				post({'num':num,'menu':menu});
+				post({'ref':ref,'menu':menu});
 			}
 						
 		});
 		
-		//post방식으로 넘기기
+		//원본 글 삭제 post방식으로 넘기기
 		function post(params){
 			var form = document.createElement("form");
 			form.setAttribute("method","POST");
-			form.setAttribute("action","./"+board+"Delete");
+			form.setAttribute("action","./qnaDelete");
 			for(var key in params){
 				var hiddenField = document.createElement("input");
 				hiddenField.setAttribute("type","hidden");
@@ -39,100 +38,36 @@
 			form.submit();
 		}
 		
-		///////////////////////////////////////////// 댓글 관련▼
-		
-		//글자수 textarea 체크
-		$('.c_area').keyup(function (e){
-		    var content = $(this).val();
-		    $('#counter').html("("+content.length+" / 500)");    //글자수 실시간 카운팅
-
-		    if (content.length > 500){
-		        alert("최대 200자까지 입력 가능합니다.");
-		        $(this).val(content.substring(0, 500));
-		        $('#counter').html("(200 / 500)");
-		    }
-		});
-		
-		
-		var curPage = 1;
-		
-		getList(curPage); //함수호출
-		
-		// 댓글 수정 코드
-		$('#updateBtn').click(function() {
-			var upContents = $('#updateContents').val();
-			var cnumId = $('#cnum').val();
-			$.post("../event/commentsUpdate", {
-				cnum : cnumId,
-				contents : upContents
-			},
-			function(data) {
-				if (data.trim() == '1') {
-					//location.reload();
-					getList(1);
-					//$('#c'+id).html(upContents);
-				} else {
-					alert('수정실패');
-				}
-			});
-		});
-		$('.commentslist').on('click', '.c_update', function() {
-			var id = $(this).attr('title');
-			var con = $('#c' + id).html();
-			$('#updateContents').val(con);
-			$('#cnum').val(id);
-		});
-		
-		//댓글 등록하기 코드
-		$('#comment_btn').click(function() {
+		/////////////////////////////////qna 답글 삭제 관련▼
+		$("#reply_delete").click(function() {
+			var result = confirm("삭제하시겠습니까?");
 			var num = $('#qnum').val();
-			var id = $('#c_writer').text();
-			var contents = $('.c_area').val();
-			$.ajax({
+			var menu = $('#qmenu').val();
+			if(result){
 				
-				url:"../comments/commentsWrite",
-				type:"POST",
-				data: {
-					num : num,
-					id : id,
-					contents : contents
-				},
-				success:function(data) {
-					if(data=='1') {
-						alert('등록성공');
-						location.reload();
-						getList(1);
-					} else {
-						alert('등록실패');
-					}
-				}
-			});
-			
+				replypost({'num':num,'menu':menu});
+			}
+						
 		});
-		//등록하기 코드 끝
 		
-		console.log(curPage);
-		//리스트 가져오기
-		function getList(curPage) {
-			$.get("../comments/commentsList?num=${vo.num}&curPage="+curPage,
-					function(data) {
-						if (curPage == 1) {
-							$('.commentslist').html(data);
-						} else {
-							$('.commentslist').append(data);
-						}
-					})
+		//답글 삭제 post방식으로 넘기기
+		function replypost(params){
+			var form = document.createElement("form");
+			form.setAttribute("method","POST");
+			form.setAttribute("action","./replyDelete");
+			for(var key in params){
+				var hiddenField = document.createElement("input");
+				hiddenField.setAttribute("type","hidden");
+				hiddenField.setAttribute("name",key);
+				hiddenField.setAttribute("value",params[key]);
+				form.appendChild(hiddenField);
+			}
+			document.body.appendChild(form);
+			form.submit();
 		}
 		
 		
-		
-		/////////////////////////////////////////////////
-				////// 댓글 더보기 코드
-				$('#more').click(function() {
-					curPage++;
-					getList(curPage);
-				});
-		
+		///////////////////////////////////////////////////////
 		
 		
 		
--- src/main/webapp/resources/js/board/boardUpdate.js
+++ src/main/webapp/resources/js/board/boardUpdate.js
@@ -35,16 +35,6 @@ $(function() {
 	 });
 	
 	
-	//qna부분 form 검증
-	$("#q_write").click(function() {
-			//다른 input들 검증
-			if($('#contents').summernote('isEmpty')) {
-				alert('모두 입력해주세요.');
-			} else {
-				$('#frm').submit();				
-			}
-		});
-	
 	
 	
 	   $('#fix_info').hide();
